e8eec9351ae0c838b54e76d0c8bdfeb403196fc2
Juan Esteban Parra Ducuara <juan.parra@syscafe.com>
2025-02-08 12:29:02 -0500

    Ajuste lista para redireccionar y cargando

--- src/pokemons/presentation/views/PokemonList.view.tsx
+++ src/pokemons/presentation/views/PokemonList.view.tsx
@@ -2,54 +2,80 @@
 import { useEffect, useState } from 'react';
 import { fetchPokemonList } from '../../domain/usecases/pokemon.usecase';
 import { Pokemon } from '../../domain/entities/Pokemon';
-import { Button, Card, CardBody, CardHeader, Image, Listbox, ListboxItem } from '@heroui/react';
+import { Button, Card, CardBody, CardHeader, Image, Listbox, ListboxItem, Skeleton } from '@heroui/react';
 import { useNavigate } from 'react-router-dom';
 
+
+const SkeletonLoader = () => {
+    return (
+        <Card className="p-4 w-1/4" radius="lg">
+            <Skeleton className="w-2/5 mb-3 rounded-lg">
+                <div className="h-3 w-2/5 rounded-lg bg-default-300" />
+            </Skeleton>
+            <Skeleton className="rounded-lg">
+                <div className="h-24 rounded-lg bg-default-300" />
+            </Skeleton>
+        </Card>
+    )
+}
 
 const PokemonList = () => {
     const navigate = useNavigate();
     const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    const getPokemonList = async () => {
+        setLoading(true);
+        const data = await fetchPokemonList();
+        setPokemonList(data);
+        setLoading(false);
+    };
 
     useEffect(() => {
-        const getPokemonList = async () => {
-            const data = await fetchPokemonList();
-            setPokemonList(data);
-        };
         getPokemonList();
     }, []);
-    
-    console.log("a",pokemonList[0]);
 
     return (
         <div>
-            <Card className='w-3/4 m-auto h-5/6 mt-10'>
+            <Card className='w-2/3 mx-auto mt-10'>
                 <CardHeader>
                     <div className='w-full grid grid-cols-7 gap-3'>
                         <span className='text-2xl font-bold col-span-6 text-center text-pink-600 ml-20'>
                             Lista Pokemons
                         </span>
-                        <Button color='success' onPress={() => navigate("/add")}>
-                            New Pokémon
+                        <Button color='success' onPress={() => getPokemonList()}>
+                            actualizar
                         </Button>
                     </div>
                 </CardHeader>
                 <CardBody>
-                    <div className='flex flex-wrap justify-center gap-8'>
-                        {pokemonList.map((pokemon) => (
-                            <Card className='p-2 w-1/4 hover:scale-105 hover:shadow-lg hover:shadow-gray-500 hover:cursor-pointer hover:text-blue-400'>
-                                <CardHeader>
-                                    <span className='text-2xl font-bold ml-auto mr-auto capitalize'>
-                                        {pokemon.name}
-                                    </span>
-                                </CardHeader>
-                                <CardBody>
-                                    <div className='flex justify-center'>
-                                        <Image src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} isBlurred/>
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        ))}
-                    </div>
+                    {
+                        loading ? (
+                            <div className='flex flex-wrap justify-center gap-8'>
+                                <SkeletonLoader />
+                                <SkeletonLoader />
+                                <SkeletonLoader />
+                            </div>
+                        ) :
+
+                            (<div className='flex flex-wrap justify-center gap-8'>
+                                {pokemonList.map((pokemon) => (
+                                    <Card className='p-2 w-1/4 hover:scale-105 hover:shadow-lg hover:shadow-gray-500 hover:cursor-pointer hover:text-blue-400' isPressable onPress={() => navigate(`/pokemon/${pokemon.name}`)}>
+                                        <CardHeader>
+                                            <span className='text-2xl font-bold ml-auto mr-auto capitalize'>
+                                                {pokemon.name}
+                                            </span>
+                                        </CardHeader>
+                                        <CardBody>
+                                            <div className='flex justify-center'>
+                                                <Image src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} isBlurred />
+                                            </div>
+                                        </CardBody>
+                                    </Card>
+                                ))}
+                            </div>)
+                    }
+
                 </CardBody>
             </Card>
 
